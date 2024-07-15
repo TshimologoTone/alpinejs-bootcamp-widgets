2272d447f38f051a37b2e89812d0136ce63055e8
Tshimologo Simon Tone <tonetshimologo@gmail.com>
2024-07-15 13:39:50 +0200

    first commit

--- index.js
+++ index.js
@@ -1,84 +1,119 @@
-function greetWidget() {
+// Transport Fee Function JS
+function transportFee() {
     return {
-        neighbourName: '',
-        greet() {
-            return 'Hello, ' + this.neighbourName;
-        }
-    }
-}
-
-function transportWidget() {
-    return {
-        shift: '',
-        transportFee(shift) {
-            switch(shift) {
-                case 'morning':
-                    return 'R20';
-                case 'afternoon':
-                    return 'R10';
-                case 'nightshift':
-                    return 'free';
-                default:
-                    return 'Invalid shift';
+        userInput: '',
+        result: '',
+        calculateFee(shift) {
+            if (shift === 'morning') {
+                return 'R20';
+            } else if (shift === 'afternoon') {
+                return 'R10';
+            } else if (shift === 'nightshift') {
+                return 'free';
+            } else {
+                return 'invalid shift selected.';
             }
         }
     }
 }
 
-function wordGameWidget() {
+document.addEventListener('alpine:init', () => {
+    Alpine.data('transportFee', transportFee);
+});
+
+
+// Total Phone Bill Function
+function phoneBillCalculator() {
     return {
-        sentence: '',
-        longestWord(sentence) {
-            const words = sentence.split(' ');
-            let longest = '';
-            for (const word of words) {
-                if (word.length >= longest.length) {
-                    longest = word;
+        numCalls: '',
+        numSMS: '',
+        result: '',
+        calculateBill() {
+            var callCount = parseInt(this.numCalls) || 0;
+            var smsCount = parseInt(this.numSMS) || 0;
+            
+            var totalCost = (callCount * 2.75) + (smsCount * 0.65);
+            
+            this.result = 'R' + totalCost.toFixed(2);
+        }
+    };
+}
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('phoneBillCalculator', phoneBillCalculator);
+});
+
+
+
+// Greeting Function
+
+function greetingApp() {
+    return {
+        name: '',
+        language: 'isiZulu',
+        result: '',
+        warning: '',
+        greetings: {
+            english: 'Hello',
+            isiZulu: 'Sawubona',
+            Tswanah: 'Dumela',
+            isiXhosa: 'Molo'
+        },
+
+        generateGreeting() {
+            if (this.name.trim() === '') {
+                this.warning = 'Please enter your name';
+                this.result = '';
+            } else {
+                this.warning = '';
+                const greeting = this.greetings[this.language];
+                this.result = `${greeting}, ${this.name}`;
+            }
+        },
+    }
+}
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('greetingApp', greetingApp);
+});
+
+
+// Enough Airtime Function
+function airtimeCalculator() {
+    return {
+        usedAirtime: '',
+        balanceAirtime: 0,
+        result: '',
+        
+        calculateRemainingAirtime() {
+            var sorted_list = this.usedAirtime.split(",");
+            var callCost = 1.88;
+            var smsCost = 0.75;
+            var dataCost = 12;
+           
+            var totalCost = 0;
+           
+            for (var i = 0; i < sorted_list.length; i++) {
+                if (sorted_list[i] === 'call') {
+                    totalCost += callCost;
+                } else if (sorted_list[i] === 'sms') {
+                    totalCost += smsCost;
+                } else if (sorted_list[i] === 'data') {
+                    totalCost += dataCost;
                 }
             }
-            return longest;
-        },
-        shortestWord(sentence) {
-            const words = sentence.split(' ');
-            let shortest = words[0];
-            for (const word of words) {
-                if (word.length <= shortest.length) {
-                    shortest = word;
-                }
+            
+            var remainingAirtime = this.balanceAirtime - totalCost;
+            
+            if (remainingAirtime > 0) { 
+                this.result = 'R' + remainingAirtime.toFixed(2);
+            } else { 
+                this.result = 'R0.00';
             }
-            return shortest;
-        },
-        wordLengths(sentence) {
-            const words = sentence.split(' ');
-            let totalLength = 0;
-            for (const word of words) {
-                totalLength += word.length;
-            }
-            return totalLength;
         }
-    }
+    };
 }
 
-function itemsWidget() {
-    return {
-        threshold: 0,
-        items: [
-            { name: 'apples', qty: 10 },
-            { name: 'pears', qty: 37 },
-            { name: 'bananas', qty: 27 },
-            { name: 'apples', qty: 3 }
-        ],
-        filteredItems: [],
-        itemsOver20: [],
-        findItemsOver20(items) {
-            return items.filter(item => item.qty > 20);
-        },
-        findItemsOver(items, threshold) {
-            return items.filter(item => item.qty > threshold);
-        },
-        findItems() {
-            this.filteredItems = this.findItemsOver(this.items, this.threshold);
-            this.itemsOver20 = this.findItemsOver20(this.items);
-        }
-    }
-}
+document.addEventListener('alpine:init', () => {
+    Alpine.data('airtimeCalculator', airtimeCalculator);
+});
